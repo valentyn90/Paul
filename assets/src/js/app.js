a45b5bad734c2a7449d6b7340c998ecb4be051b8
itsumrat <itsumrat@gmail.com>
2017-02-09 12:44:41 +0600

    all sections added

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -1,9 +1,56 @@
 jQuery(document).ready(function($){
 
-	$('.navicon').on('click', function (e) {
-	  e.preventDefault();
-	  $(this).toggleClass('navicon--active');
-	  $('.toggle').toggleClass('toggle--active');
-	});
+    // Navigation
+        // Closes the sidebar menu
+    $("#menu-close").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+    // Opens the sidebar menu
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+ 
+    // handle links with @href started with '#' only
+    $(document).on('click', 'a[href^="#"]', function(e) {
+        // target element id
+        var id = $(this).attr('href');
+
+        // target element
+        var $id = $(id);
+        if ($id.length === 0) {
+            return;
+        }
+
+        // prevent standard hash navigation (avoid blinking in IE)
+        e.preventDefault();
+
+        // top position relative to the document
+        var pos = $(id).offset().top;
+
+        // animated top scrolling
+        $('body, html').animate({scrollTop: pos}, 2000);
+    });
+
+    // Testimonial
+	$("#testimonial-slider").owlCarousel({
+        items:1,
+        itemsDesktop:[1000,2],
+        itemsDesktopSmall:[979,1],
+        itemsTablet:[768,1],
+        pagination:false,
+        navigation:true,
+        slideSpeed:1000,
+        singleItem:true,
+        transitionStyle:"goDown",
+        navigationText:["",""],
+        autoPlay:false
+    });
 
 });
+
+window.onload = function() {
+    zoomwall.create(document.getElementById('gallery'));
+};
+
